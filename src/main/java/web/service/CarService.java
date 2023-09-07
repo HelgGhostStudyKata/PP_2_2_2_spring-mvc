0fd6c8495d28ed71af31ca2f1357c9525cd19a41
HelgGhost <helgghost@yandex.ru>
2023-09-07 10:19:56 +0700

    1

--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,22 +1,9 @@
 package web.service;
 
-import org.springframework.stereotype.Service;
 import web.model.Car;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-@Service
-public class CarService {
-    private Car[] cars = {  new Car("Bugatti", 2, "red"),
-            new Car("Ferrari", 1, "blue"),
-            new Car("Alfa-romeo", 4, "white"),
-            new Car("BMV", 1, "greeen"),
-            new Car("Khodro", 1, "grey")};
-    public List<Car> getListCarCount(Integer count) {
-        count = (count == null) ? cars.length : (count < 0) ? 0 : count;
-        List<Car> listCars= new ArrayList<>(Arrays.asList(cars));
-        return listCars.subList(0, count);
-    }
+public interface CarService {
+    public List<Car> getListCarCount(Integer count);
 }
---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,23 @@
+package web.service;
+
+import org.springframework.stereotype.Service;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class CarServiceImpl implements CarService {
+    private Car[] cars = {new Car("Bugatti", 2, "red"),
+            new Car("Ferrari", 1, "blue"),
+            new Car("Alfa-romeo", 4, "white"),
+            new Car("BMV", 1, "greeen"),
+            new Car("Khodro", 1, "grey")};
+    @Override
+    public List<Car> getListCarCount(Integer count) {
+        count = (count == null) ? cars.length : (count < 0) ? 0 : count;
+        List<Car> listCars = new ArrayList<>(Arrays.asList(cars));
+        return listCars.subList(0, count);
+    }
+}
